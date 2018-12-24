e0b48d957a0a89de1918f1ab5ee2f6b2a3d4b566
Rodrigo Muñoz <rmunozdelaporte@gmail.com>
2018-12-24 18:39:17 -0300

    Heroku DJango Tool added correctly

--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,7 @@ import os
 import django_heroku
 
 
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,10 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "templates"),
-)   
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+django_heroku.settings(locals())
